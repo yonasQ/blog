944262c97dcb24d1842ff4f301d62e706f96139e
yonas kemon <yonaskemon01@gmail.com>
2021-06-19 02:04:21 +0100

    env file

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
- const withCSS=require('@zeit/next-css')
- 
- module.exports=withCSS({
-     publicRuntimeConfig:{
-         APP_NAME:"SEOBLOG",
-         API_PRODUCTION:"https://quest-blog.herokuapp.com/api",
-         API_DEVELOPMENT:"http://localhost:8000/api",
-         PRODUCTION:true,
-         DOMAIN_DEVELOPMENT:"http://localhost:3000",
-         DOMAIN_PRODUCTION:"http://localhost:3000",
-         DISQUS_SHORTNAME:'blog-7690exkuju'
-     }
- })
+
+const withCSS = require('@zeit/next-css')
+
+module.exports = withCSS({
+    cssModules: true,
+    publicRuntimeConfig: {
+        DISQUS_SHORTNAME : process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
+        APP_NAME :process.env.NEXT_PUBLIC_APP_NAME,
+        DOMAIN : process.env.NEXT_PUBLIC_PRODUCTION ? process.env.NEXT_PUBLIC_DOMAIN_PRODUCTION : process.env.NEXT_PUBLIC_DOMAIN_DEVELOPMENT,
+        API : process.env.NEXT_PUBLIC_PRODUCTION ? process.env.NEXT_PUBLIC_API_PRODUCTION : process.env.NEXT_PUBLIC_API_DEVELOPMENT,
+    },
+})
